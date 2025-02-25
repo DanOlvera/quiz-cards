7801bbf0894a0e5040e9407589d175dedbf54001
Daniel Olvera <ing.josuedanielescamilla@gmail.com>
2025-02-25 08:22:28 -0800

    Added repository to manage data access and manipulation

--- app/src/main/java/com/example/quizcards/feat_android_quiz/model/repo/QuizRepository.kt
+++ app/src/main/java/com/example/quizcards/feat_android_quiz/model/repo/QuizRepository.kt
@@ -1,4 +1,19 @@
 package com.example.quizcards.feat_android_quiz.model.repo
 
-class QuizRepository {
+import com.example.quizcards.feat_android_quiz.model.network.ApiQuizService
+import com.example.quizcards.feat_android_quiz.state.QuizUiState
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+
+class QuizRepository(private val apiQuizService: ApiQuizService) {
+
+    fun getAndroidQuiz(): Flow<QuizUiState> = flow {
+        try {
+            emit(QuizUiState.Loading)
+            val response = apiQuizService.getAndroidQuiz()
+            emit(QuizUiState.Success(response))
+        } catch (e: Exception) {
+            emit(QuizUiState.Error(e.message ?: "An error occurred"))
+        }
+    }
 }
